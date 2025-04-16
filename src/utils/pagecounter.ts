9439251197fcbfb12153542d981974e880799b4d
The chicken  MAN <856030@dpsk12.net>
2025-04-16 15:06:55 -0600

    my new cool commit message

--- src/utils/pagecounter.ts
+++ src/utils/pagecounter.ts
@@ -1,16 +1,23 @@
 function pageViewCounter(): void {
+  
+  let viewCount: string | null = localStorage.getItem('pageViewCount');
 
-  let viewCount: number | null = localStorage.getItem('pageViewCount');
-viewCount = viewCount ? parseInt(viewCount, 10) : 0;
+  
+  viewCount = viewCount ? parseInt(viewCount, 10) : 0;
+
+  
+  viewCount += 1;
 
   
   localStorage.setItem('pageViewCount', viewCount.toString());
+
 
   const viewCounterElement = document.getElementById('view-counter');
   if (viewCounterElement) {
     viewCounterElement.textContent = viewCount.toString();
   }
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
   pageViewCounter();
