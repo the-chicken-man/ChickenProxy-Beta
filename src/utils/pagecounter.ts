5d8b22d5597b9eed5440ad89b92a2429fb73c629
The chicken  MAN <856030@dpsk12.net>
2025-04-16 15:04:18 -0600

    my cool commit name

--- src/utils/pagecounter.ts
+++ src/utils/pagecounter.ts
@@ -1,17 +1,17 @@
 function pageViewCounter(): void {
-    let viewCount: number | null = localStorage.getItem('pageViewCount');
+
+  let viewCount: number | null = localStorage.getItem('pageViewCount');
+viewCount = viewCount ? parseInt(viewCount, 10) : 0;
+
   
-    if (viewCount) {
-      viewCount = parseInt(viewCount) + 1;
-    } else {
-      viewCount = 1;
-    }
-  
-    localStorage.setItem('pageViewCount', viewCount.toString());
-    const viewCounterElement = document.getElementById('view-counter');
-    if (viewCounterElement) {
-      viewCounterElement.textContent = viewCount.toString();
-    }
+  localStorage.setItem('pageViewCount', viewCount.toString());
+
+  const viewCounterElement = document.getElementById('view-counter');
+  if (viewCounterElement) {
+    viewCounterElement.textContent = viewCount.toString();
   }
-  
-  document.addEventListener('DOMContentLoaded', pageViewCounter);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  pageViewCounter();
+});
